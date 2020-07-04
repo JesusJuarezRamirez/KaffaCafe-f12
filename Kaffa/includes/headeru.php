3146092f896bc29728cfaef8fb1314a10c349393
jesus-jesus-ramirez <59908021+jesus-jesus-ramirez@users.noreply.github.com>
2020-07-04 13:01:39 -0500

    Add files via upload

---
+++ Kaffa/includes/headeru.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio Kaffa Café</title>
+
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/jquery.glide.js"></script>
+
+    <link rel="stylesheet" href="../assets/css/ustyle.css">
+    <link rel="stylesheet" href="../assets/css/animate.css">
+    <script type="text/javascript" src="../assets/js/MyJQu.js"></script>
+    <script src="../assets/js/jquery.localScroll.min.js" type="text/javascript"></script>
+    <script src="../assets/js/jquery.scrollTo.min.js" type="text/javascript"></script>
+    <script src="../assets/js/wow.min.js" type="text/javascript"></script>
+
+    <!-- función  de desplazar -->
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#navigations').localScroll({
+                duration: 800
+            });
+        });
+    </script>
+
+
+    <script src="../assets/js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+</head>
